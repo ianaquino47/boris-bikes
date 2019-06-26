2abfd5981166e3d0c64d849cd01aceb0f2a9576d
iAquino47 <ianaquino47@gmail.com>
2019-06-26 14:58:47 +0100

    finished stage 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
+require 'pry'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
 end
+
+bike = Bike.new
+docking_station = DockingStation.new
+binding.pry
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,22 @@ describe DockingStation do
   it 'gets a bike' do
     bike = subject.release_bike
     expect(bike).to be_working
-    # expect(subject.release_bike).to eq true
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to :bike }
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'shows docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+
 end
